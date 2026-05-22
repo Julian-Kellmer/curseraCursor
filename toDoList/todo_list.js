c3577a165aa091f2dba7f144b989e7b5b44f2d81
julian-kellmer <julian-kellmer>
2026-05-22 11:19:36 -0400

    todolist donde

---
+++ toDoList/todo_list.js
@@ -0,0 +1,45 @@
+const taskInput = document.getElementById('taskInput')
+
+const addTaskBtn =document.getElementById('addTaskButton');
+const taskList = document.getElementById('taskList');
+const clearCompletedBtn =document.getElementById('clearCompletedBtn');
+
+let tasks =[]
+
+function addTask(){
+    const taskText = taskInput.value.trim();
+    if(taskText !== ''){
+        tasks.push({text:taskText})
+        taskInput.value= ''
+        console.log("esta es la tarea que se agreu" ,taskText)
+        displayTasks()
+        console.log("estas son las tareas",tasks)
+    }
+}
+
+function displayTasks() {
+    taskList.innerHTML = '';
+    tasks.forEach((task, index) => {
+        const li = document.createElement("li");
+        
+        // Corrección: Acomodamos las comillas del id y el atributo checked
+        li.innerHTML = `<input type="checkbox" id="task-${index}" ${task.completed ? "checked" : ""}>
+        <label for="task-${index}"> ${task.text}</label>`;
+        
+        li.querySelector("input").addEventListener("change", () => toggleTask(index));
+        taskList.appendChild(li);
+    });
+}
+
+function toggleTask(index){
+    tasks[index].completed=!tasks[index].completed
+    displayTasks()
+} 
+
+function clearCompletedTasks(){
+    tasks  = tasks.filter(task => !task.completed)
+    displayTasks()
+}
+
+addTaskBtn.addEventListener("click", addTask);
+clearCompletedBtn.addEventListener("click", clearCompletedTasks); 
